bebf6306601ce4da85cc2498b53a5eaf939587de
Noi-Git <noi.patsin@gmail.com>
2024-05-25 22:01:55 -0700

    pass params using next router navigation

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -6,13 +6,20 @@ import {
   searchInput,
 } from '@/components/styles/navigationStyle'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const SearchBar = () => {
+  const router = useRouter()
+
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    const formData = new FormData(e.currentTarget)
-    const name = formData.get('name')
+    const formData = new FormData(e.currentTarget) //get value user type
+    const name = formData.get('name') as string
+
+    if (name) {
+      router.push(`/list?name=${name}`)
+    }
   }
   return (
     <form className={searchForm} onSubmit={handleSearch}>
