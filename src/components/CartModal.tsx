8fa7772f2061e95eb07f40d2d81c51a9f1967e17
Noi-Git <noi.patsin@gmail.com>
2024-05-27 12:10:13 -0700

    add product image to the cart

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,10 +1,37 @@
 'use client'
 
 import React from 'react'
-import { navCartModal } from './styles/navigationStyle'
+import { cartImage, navCartModal } from './styles/navigationStyle'
+import Image from 'next/image'
 
 const CartModal = () => {
-  return <div className={navCartModal}>CartModal</div>
+  const cartItems = true
+
+  return (
+    <div className={navCartModal}>
+      {!cartItems ? (
+        <div className=''>Cart is Empty</div>
+      ) : (
+        <div className=''>
+          <Image
+            src='https://m.media-amazon.com/images/I/7176IGSqbCL._AC_UL480_FMwebp_QL65_.jpg'
+            alt=''
+            width={72}
+            height={96}
+            className={cartImage}
+          />
+          <div className=''>
+            {/* TOP SECTION */}
+            <div className=''>
+              <h3>Product Name</h3>
+              <div className=''>$49</div>
+            </div>
+            {/* BOTTOM SECTION */}
+          </div>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default CartModal
