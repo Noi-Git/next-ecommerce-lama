ec7e6c83630a33f38608052a4252efce2422129f
Noi-Git <noi.patsin@gmail.com>
2024-05-27 21:02:05 -0700

    add sample products

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,40 +1,122 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import {
+  productButton,
+  productCard,
+  productCardLink,
+  productContainer,
+  productDescription,
+  productImg1,
+  productImg2,
+  productInfoContainer,
+  productInfoTitle,
+  productPrice,
+} from './styles/productStyle'
 
 const ProductList = () => {
   return (
-    <div className='flex gap-8 gap-y-16 justify-between flex-wrap mt-12'>
+    <div className={productContainer}>
       {/* when use fill -- the parent need to be relative */}
-      <Link
-        href='/test'
-        className='w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]'
-      >
-        <div className='relative w-full h-80'>
+      <Link href='/test' className={productCardLink}>
+        <div className={productCard}>
+          <Image
+            src='https://images.pexels.com/photos/6616146/pexels-photo-6616146.jpeg?auto=compress&cs=tinysrgb&w=600'
+            alt=''
+            fill
+            sizes='25vw'
+            className={productImg1}
+          />
+          <Image
+            src='https://images.pexels.com/photos/5893849/pexels-photo-5893849.jpeg?auto=compress&cs=tinysrgb&w=600'
+            alt=''
+            fill
+            sizes='25vw'
+            className={productImg2}
+          />
+        </div>
+        <div className={productInfoContainer}>
+          <span className={productInfoTitle}>Product Name</span>
+          <span className={productPrice}>$49</span>
+        </div>
+        <div className={productDescription}>My description</div>
+        <button className={productButton}>Add to card</button>
+      </Link>
+
+      {/* BEGINING OF EXAMPLE */}
+      <Link href='/test' className={productCardLink}>
+        <div className={productCard}>
           <Image
             src='https://images.pexels.com/photos/45982/pexels-photo-45982.jpeg?auto=compress&cs=tinysrgb&w=600'
             alt=''
             fill
             sizes='25vw'
-            className='absolute object-cover rounded-md z-10 hover:opacity-0 transistion-opacity easy duration-500'
+            className={productImg1}
           />
           <Image
             src='https://images.pexels.com/photos/2062324/pexels-photo-2062324.jpeg?auto=compress&cs=tinysrgb&w=600'
             alt=''
             fill
             sizes='25vw'
-            className='absolute object-cover rounded-md'
+            className={productImg2}
           />
         </div>
-        <div className='flex justify-between'>
-          <span className='font-medium'>Product Name</span>
-          <span className='font-semibold'>$49</span>
+        <div className={productInfoContainer}>
+          <span className={productInfoTitle}>Product Name</span>
+          <span className={productPrice}>$49</span>
         </div>
-        <div className='text-sm text-gray-500'>My description</div>
-        <button className='rounded-2xl ring-1 ring-pink text-pink py-2 px-4 text-xs hover:bg-pink hover:text-white w-max'>
-          Add to card
-        </button>
+        <div className={productDescription}>My description</div>
+        <button className={productButton}>Add to card</button>
       </Link>
+      <Link href='/test' className={productCardLink}>
+        <div className={productCard}>
+          <Image
+            src='https://images.pexels.com/photos/9594148/pexels-photo-9594148.jpeg?auto=compress&cs=tinysrgb&w=600'
+            alt=''
+            fill
+            sizes='25vw'
+            className={productImg1}
+          />
+          <Image
+            src='https://images.pexels.com/photos/9594142/pexels-photo-9594142.jpeg?auto=compress&cs=tinysrgb&w=600'
+            alt=''
+            fill
+            sizes='25vw'
+            className={productImg2}
+          />
+        </div>
+        <div className={productInfoContainer}>
+          <span className={productInfoTitle}>Product Name</span>
+          <span className={productPrice}>$49</span>
+        </div>
+        <div className={productDescription}>My description</div>
+        <button className={productButton}>Add to card</button>
+      </Link>
+      <Link href='/test' className={productCardLink}>
+        <div className={productCard}>
+          <Image
+            src='https://images.pexels.com/photos/14410035/pexels-photo-14410035.jpeg?auto=compress&cs=tinysrgb&w=600'
+            alt=''
+            fill
+            sizes='25vw'
+            className={productImg1}
+          />
+          <Image
+            src='https://images.pexels.com/photos/15181108/pexels-photo-15181108/free-photo-of-woman-in-traditional-bridal-saree-dress.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+            alt=''
+            fill
+            sizes='25vw'
+            className={productImg2}
+          />
+        </div>
+        <div className={productInfoContainer}>
+          <span className={productInfoTitle}>Product Name</span>
+          <span className={productPrice}>$49</span>
+        </div>
+        <div className={productDescription}>My description</div>
+        <button className={productButton}>Add to card</button>
+      </Link>
+      {/* END OF EXAMPLE */}
     </div>
   )
 }
---
+++ src/components/styles/productStyle.ts
@@ -0,0 +1,15 @@
+export const productContainer =
+  'flex gap-8 gap-y-16 justify-between flex-wrap mt-12'
+export const productCardLink =
+  'w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]'
+export const productCard = 'relative w-full h-80'
+export const productImg1 =
+  'absolute object-cover rounded-md z-10 hover:opacity-0 transistion-opacity easy duration-500'
+export const productImg2 = 'absolute object-cover rounded-md'
+
+export const productInfoContainer = 'flex justify-between'
+export const productInfoTitle = 'font-medium'
+export const productPrice = 'font-semibold'
+export const productDescription = 'text-sm text-gray-500'
+export const productButton =
+  'rounded-2xl ring-1 ring-pink text-pink py-2 px-4 text-xs hover:bg-pink hover:text-white w-max'
